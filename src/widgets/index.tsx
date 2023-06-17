430ed43cc124ce42c3cfb1a901b575c2b634080e
brexe junior <brexejunior@gmail.com>
2023-06-17 17:16:46 +0300

    add component product

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,8 +1,8 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import ListCategorie from "./components/ListCategorie/ListCategorie";
-import ListPopularCategorie from "./components/ListPopularCategorie/ListPopularCategorie";
-import ListArticle from "./components/ListArcticle/ListArticle";
+import ListCategorie from "./product/components/ListCategorie/ListCategorie";
+import ListPopularCategorie from "./product/components/ListPopularCategorie/ListPopularCategorie";
+import ListArticle from "./article/components/ListArcticle/ListArticle";
 import ListAdvantage from "./components/ListAdvantages/ListAdvantage";
 import ListAdvertisement from "./components/ListAdvertisement/ListAdvertisement";
 
@@ -15,6 +15,4 @@ export {
     ListArticle,
     ListAdvantage,
     ListAdvertisement
-
-    
 }
